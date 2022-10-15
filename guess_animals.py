2f41dfb6e846472c3ea8543499da1c4d33bc2a8c
PlsHeal <stechnology357@gmail.com>
2022-10-16 00:46:03 +0800

    Add guess_animals.py

---
+++ guess_animals.py
@@ -0,0 +1,17 @@
+from pickle import FALSE
+import random
+
+animals = ["dog", "cat", "rabbit"]
+
+def guessAnimal():
+    isGuess = True
+    number = random.randint(0, 2)
+    while (isGuess):
+        answer = input("What is your answer? ")
+        if (animals[number] == answer):
+            print("Good Job, you guess it!")
+            isGuess = False
+        elif (animals[number] != answer):
+            print('Bad guess')
+    
+guessAnimal()
